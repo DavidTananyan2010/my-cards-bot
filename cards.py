e8986d04359826c9ae7f4cbe3ad1264fec58feed
DavidTananyan2010 <tananyandavid5@gmail.com>
2026-06-16 11:52:52 +0400

    Add files via upload

---
+++ cards.py
@@ -0,0 +1,16 @@
+REAL_CARDS = [
+    {"file": "3.jpg", "name": "金 sunny🌲 김지ха | DA 🐂", "rarity": "⭐ Обычная", "price": 10},
+    {"file": "5.jpg", "name": "Солнечный Самурай ☀️", "rarity": "⭐ Обычная", "price": 10},
+    {"file": "4.jpg", "name": "Меха-Бык 🐂", "rarity": "⭐ Редкая", "price": 30},
+
+    {"file": "1.jpg", "name": "👻losnya🐂🌲", "rarity": "🔮 Секретная", "price": 100},
+    {"file": "7.jpg", "name": "Призрак Леса 👻", "rarity": "⭐ Редкая", "price": 30},
+    {"file": "6.jpg", "name": "Таинственный Лось 🦌", "rarity": "🔮 Секретная", "price": 100},
+
+    {"file": "2.jpg", "name": "Дониёр 🌲", "rarity": "🌌 Секретная", "price": 100},
+    {"file": "9.jpg", "name": "Страж Дубравы 🌲", "rarity": "⭐ Обычная", "price": 10},
+    {"file": "8.jpg", "name": "Лесной Хакер 💻", "rarity": "⭐ Редкая", "price": 30}
+]
+
+EMPTY_CARDS = [{"file": None, "name": "Эта карта пуста, открой ещё 😔", "rarity": "⚪ Пустышка", "price": 0}] * 10
+CARDS = REAL_CARDS + EMPTY_CARDS
