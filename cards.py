f9a84cede0fc1d873a2383094feb197452a1d0a1
DavidTananyan2010 <tananyandavid5@gmail.com>
2026-06-16 15:29:14 +0400

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,13 +1,13 @@
 REAL_CARDS = [
-    {"file": "1.jpg", "name": "金 sunny🌲 김지ха | DA 🐂", "rarity": "🔮 секретная", "price": 100},
+    {"file": "2.jpg", "name": "金 sunny🌲 김지ха | DA 🐂", "rarity": "🔮 секретная", "price": 100},
     {"file": "5.jpg", "name": "Солнечный Самурай ☀️", "rarity": "⭐ Обычная", "price": 10},
     {"file": "4.jpg", "name": "Меха-Бык 🐂", "rarity": "⭐ Редкая", "price": 30},
 
-    {"file": "2.jpg", "name": "👻losnya🐂🌲", "rarity": "🔮 Секретная", "price": 100},
+    {"file": "3.jpg", "name": "👻losnya🐂🌲", "rarity": "🔮 Секретная", "price": 100},
     {"file": "7.jpg", "name": "Призрак Леса 👻", "rarity": "⭐ Редкая", "price": 30},
     {"file": "6.jpg", "name": "Таинственный Лось 🦌", "rarity": "🔮 Секретная", "price": 100},
 
-    {"file": "3.jpg", "name": "Дониёр 🌲", "rarity": "🔮 Секретная", "price": 100},
+    {"file": "1.jpg", "name": "Дониёр 🌲", "rarity": "🔮 Секретная", "price": 100},
     {"file": "9.jpg", "name": "Страж Дубравы 🌲", "rarity": "⭐ Обычная", "price": 10},
     {"file": "8.jpg", "name": "Лесной Хакер 💻", "rarity": "⭐ Редкая", "price": 30}
 ]
